51b817dace9fd8456baf2f7fa3d4def2549a2cba
Brian Miyaji <brian@themeboy.com>
2016-02-18 01:01:55 +1100

    Load parent theme style

--- functions.php
+++ functions.php
@@ -14,3 +14,8 @@ if ( ! isset( $content_width ) ) {
 if ( ! isset( $full_content_width ) ) {
 	$full_content_width = 960; /* pixels */
 }
+
+function rookie_child_scripts() {
+	wp_enqueue_style( 'rookie-parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'rookie_child_scripts', 11 );
